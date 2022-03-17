c67de54be4827649ede119d6dfdd0251ee437161
LiamSG02 <liamswansson@gmail.com>
2022-03-17 20:34:16 +0100

    Header Component JSX

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+        <a href="https://www.linkedin.com/in/liamsg02/" target="_blank"></a>
+        <a href="https://www.linkedin.com/in/liamsg02/" target="_blank"></a>
+        <a href="https://www.linkedin.com/in/liamsg02/" target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
