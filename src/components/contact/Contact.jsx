27aa0a8e258fd0ab5dfd2309a8572045b248e6bc
LiamSG02 <liamswansson@gmail.com>
2022-03-21 18:26:24 +0100

    Contact Component CSS

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,19 +13,19 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail />
+            <MdOutlineEmail className='contact__option-icon' />
             <h4>Email</h4>
             <h5>Liamswansson@gmail.com</h5>
             <a href="mailto:Liamswansson@gmail.com" target="_blank">Send a message</a>
           </article>
           <article className="contact__option">
-            <RiMessengerLine />
+            <RiMessengerLine className='contact__option-icon' />
             <h4>Messenger</h4>
             <h5>Liam</h5>
             <a href="#" target="_blank">Send a message</a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className='contact__option-icon' />
             <h4>WhatsApp</h4>
             <h5>+123456789</h5>
             <a href="#" target="_blank">Send a message</a>
